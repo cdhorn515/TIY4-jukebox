6f400777278834a8a2ca21108c8b55ef28237b71
Christina Cornell <cdhorn515@gmail.com>
2017-06-17 22:38:45 -0400

    working on function to get input stored in variable

--- main.js
+++ main.js
@@ -1,50 +1,78 @@
 (function() {
-  'use strict';
+    'use strict';
 
-var tracksUrl= 'https://api.soundcloud.com/tracks/13158665?client_id=8538a1744a7fdaa59981232897501e04'
-// var headers = {};
-// 1. First select and store the elements you'll be working with
-var containerNode = document.getElementById('container');
+    var tracksUrl = 'https://api.soundcloud.com/tracks/13158665?client_id=8538a1744a7fdaa59981232897501e04'
+    // var headers = {};
+    // 1. First select and store the elements you'll be working with
+    var containerNode = document.getElementById('container');
 
-var playerSectionNode = document.querySelector('.player');
-var searchParameter;
-var playerNode = document.querySelector('.music-player');
+    var playerSectionNode = document.querySelector('.player');
+    var searchParameter;
+    var playerNode = document.querySelector('.music-player');
+    var formNode = document.getElementById('search-form');
+    var searchButtonNode = document.getElementById('artistSearchBtn');
 
-//gets the form element that eventListener will go on
-var searchSectionNode = document.querySelector('.search-form');
-var searchInputNode = document.querySelector('.searchInput');
-  if (searchInputNode.length > 0){
-  var searchParameter = searchInputNode.elements('artistSearch').value;
-  console.log(searchParameter);
-  return searchParameter;
-  }
-// searchSectionNode.getAttribute('class', 'onsubmit');
-// searchSectionNode.addEventListener('click', testingFunction);
+    function saveInput(e) {
+       e.preventDefault();
+       var inputValue = document.getElementById('artistSearchInput').value;
+       console.log(inputValue);
+       alert('you entered: ', inputValue);
+     }
 
-  function testingFunction() {
-    e.preventDefault();
-    alert('getting info');
-  }
+    formNode.addEventListener('onsubmit', saveInput);
 
 
 
+  // function saveInput() {
+
+  // formNode.onsubmit = function() {
 
 
-var searchInputNode = document.querySelector('.searchInput');
-
-var searchButtonNode = document.getElementById('artistSearchBtn');
+    // var searchInputNode = document.getElementById('artistSearchInput')
 
 
 
-// 2. Create your `onSubmit` event for getting the user's search term
 
 
-// 3. Create your `fetch` request that is called after a submission
+
+    //gets the form element that eventListener will go on
+
+    // function testingFunction(e){
+    //   e.preventDefault();
+    //   console.log();
+    //   var inputValue = document.getElementById('artistSearchInput').value;
+    //   console.log(inputValue);
+    // }
 
 
-// 4. Create a way to append the fetch results to your page
 
 
-// 5. Create a way to listen for a click that will play the song in the audio play
+    // });
+    // searchSectionNode.getAttribute('class', 'onsubmit');
+    // searchSectionNode.addEventListener('click', testingFunction);
 
-}());
+
+    // function testingFunction(e) {
+    //   e.preventDefault();
+    //   alert('getting info');
+    // }
+
+
+
+
+
+
+
+
+    // 2. Create your `onSubmit` event for getting the user's search term
+
+
+    // 3. Create your `fetch` request that is called after a submission
+
+
+    // 4. Create a way to append the fetch results to your page
+
+
+    // 5. Create a way to listen for a click that will play the song in the audio play
+
+  }());
