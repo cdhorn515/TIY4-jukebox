219d0af49d34d5a3484831b1687e45c04683a6fe
Christina Cornell <cdhorn515@gmail.com>
2017-06-22 17:41:21 -0400

    music player working, search bar clears out when clicked on, new search results added to top of page

--- main.js
+++ main.js
@@ -8,58 +8,39 @@
   const CLIENT_ID = '8538a1744a7fdaa59981232897501e04';
   var containerNode = document.getElementById('container');
   var formInput;
-  // var redirectUrl = 'http://api.soundcloud.com/resolve';
   var playerSectionNode = document.querySelector('.player');
   var playerNode = document.querySelector('.music-player');
   var formNode = document.getElementById('search-form');
   var searchInput = document.getElementById('artistSearchInput');
+  //clear input value
+  searchInput.addEventListener('click', function(e){
+    searchInput.value = '';
+  });
   var searchButtonNode = document.getElementById('artistSearchBtn');
 
   formNode.addEventListener('submit', saveInput);
-
-  // FIXME:
-  //when you click on the search box it clears out any previous search
-  // searchInput.addEventListener('click', function() {
-  //   searchInput.textContent = '';
-  //   var fetchUrl = ''
-  // });
-  //flex container nodes for results of api call:
-  // console.log(CLIENT_ID);
   function saveInput(e) {
     e.preventDefault();
     formInput = e.srcElement.childNodes[1].value;
     var url = 'https://api.soundcloud.com/tracks/';
-    //  fetchUrl = url + CLIENT_ID + formInput;
-    console.log(e);
-    //  var inputValue = document.getElementById('artistSearchInput').value;
-    //  console.log(inputValue);
     formInput = formInput.toString();
-    console.log('formInput: ' + formInput);
-    console.log(formInput.toString());
-    // console.log(url + formInput);
-    //  alert('you entered: ' + formInput);
     submitFetch(url, formInput);
   }
 
   function submitFetch(url, formInput) {
     var fetchUrl = url + '?client_id=' + CLIENT_ID + '&q=' + formInput;
-    console.log(fetchUrl);
-    // var fetchUrl = url + clientId + formInput + ;
-    // console.log(fetchUrl);
-    // console.log(typeof(formInput));
     fetch(fetchUrl).then(function(response) {
       response.json().then(function(info) {
         console.log(info);
         var resultsNode = document.querySelector('.results');
-        var result = {};
-        // loop through data and create divs
+        // loop through data and create elements
         for (var i = 0; i < info.length; i++) {
-          result = info[i];
-
+           var result = info[i];
+          //container that holds pic and song title
           var returnedResults = document.createElement('div');
           returnedResults.setAttribute('class', 'flex-item');
           resultsNode.prepend(returnedResults);
-
+          //div that hold picture
           var picNode = document.createElement('div');
           picNode.setAttribute('class', 'picture');
           if (result.artwork_url) {
@@ -69,32 +50,32 @@
           }
           returnedResults.appendChild(picNode);
 
-          // songTitleNode.appendChild(stream);
-          var songTitleNode = document.createElement('a');
-          // playerNode.setAttribute('src', result.stream_url + "?client_id=" + CLIENT_ID);
-          var stream = result.stream_url + "?client_id=" + CLIENT_ID;
-          songTitleNode.setAttribute('href', stream);
-          returnedResults.appendChild(songTitleNode);
-
           var songTitle = document.createElement('p');
           songTitle.setAttribute('class', 'song');
-          // songTitle.setAttribute('id', "songStream");
-          // console.log('id' + result.stream_url);
+          var stream = result.stream_url + "?client_id=" + CLIENT_ID;
+          songTitle.setAttribute('id', stream);
           songTitle.textContent = result.title;
-          songTitleNode.appendChild(songTitle);
+          returnedResults.appendChild(songTitle);
 
           songTitle.addEventListener('click', function(e) {
-            // FIXME:
-            // alert('stream url= ' + stream);
-            playerNode.src = clickedSong + "?client_id=" + CLIENT_ID;
-            playerNode.src = e.target.href.stream;
-            console.log(playerNode.src);
+            // playerNode.setAttribute('src', e.target.href);
+            console.log(arguments);
+            getSongUrl(e.target.id);
           });
-        };
+        }
       });
 
     });
     var fetchUrl = '';
+    formInput = '';
+
   }
+function getSongUrl(url){
+  fetch(url).then(function(result){
+    console.log(result);
+    playerNode.src= result.url;
+    playerNode.play();
+  });
+}
 
 }());
